74eeeabc88b93b2f4e0e20a9ec4c9c17b2da3664
Ikelamps <ikelamps@gmail.com>
2023-01-24 19:32:50 +0000

    Python Classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Square"""
+
+
+class Square:
+    """Rep a Square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - reps the size of the square
+        """
+
+        self.__size = size
