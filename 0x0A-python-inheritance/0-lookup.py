daaf5531e5260dfd888b7a54a5f11ebfa6842480
Ikelamps <ikelamps@gmail.com>
2023-02-06 21:24:09 +0000

    Py Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Module returns the list of available attributes
+    and methods of an object
+"""
+
+
+def lookup(obj):
+    """Functions looks out for all attributes and methods of an object"""
+    return dir(obj)
